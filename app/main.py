7baa590c5d8baf5f8e952d14347b2c4d3cc07623
Jverbist <jornt@bist.be>
2024-01-11 23:16:40 +0100

    Added library's to requirements file

--- app/main.py
+++ app/main.py
@@ -1,12 +1,46 @@
 from fastapi import FastAPI
+from fastapi import Request, Depends
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from db.database import SessionLocal, engine
 
+
+
+
+# Import all the required libraries:
+
+
+
+
+
+# app code starts here:
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+# Dependencies:
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# app gets executed here:
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def home_page(request: Request, db: SessionLocal = Depends(get_db)):
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-#Test save
+    return templates.TemplateResponse("home/index.html", {"request": request})
+
+@app.get("/game")
+async def game_page(request: Request, db: SessionLocal = Depends(get_db)):
+
+    return templates.TemplateResponse("home/index.html", {"request": request})
+# app posts execution code here:
+
+
+
+
+
+
